8b2c7248bce3eb84e5459e578207fc3f0824b839
Satzya <satyabratachakraborty275@gmail.com>
2019-04-16 22:25:18 +0530

    'Latest'

--- Sort.js
+++ Sort.js
@@ -1,27 +1,58 @@
-console.log('HAHJKJNMNMNMNMA');
-alert('Radio done');
+// console.log('HAHJKJNMNMNMNMA');
+// // alert('Radio done');
 
-(function jkl() {
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    console.log('POLLINGS');
-    function mno() {
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-        console.log('POLLINGS');
-    }
+// var cpp = {
+//     'haha': 'baba', 'kaka': 'lala'
+// }
+
+// console.log(cpp);
+
+// () => {
+//     console.log('hopp'); console.log('POLLINGS'); console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+// }
+
+// (function jkl() {
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('POLLINGS');
+//     console.log('Locco')
+//     function mno() {
+//         console.log('POLLINGS')
+//         console.log('POLLINGS');
+//         console.log('POLLINGS');
+//         console.log('POLLINGS');
+//         console.log('POLLINGS');
+//         console.log('POLLINGS');
+//         console.log('POLLINGS');
+//     }
+// }())
+
+// console.log('HAHA');
+// var e;
+// var gh;
+// var cd;
+
+// function mno(oo) {
+//     oo = 'ttl';
+// }
+
+// function lmn(oo) {
+//     oo = 'ttl';
+// }
+
+(function pp() {
+    let j = 1;
+    setInterval(() => {
+        
+            console.log(j++);
+        
+    }, 1999);
 }());
-
-console.log('HAHA');
-var e;
-var gh;
-var cd;
